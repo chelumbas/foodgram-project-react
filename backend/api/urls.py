d4e8fd53b298fc278dd6dad5d71b9026d168038f
Dmitriy Chelumbasov <d.chelumbasov@gmail.com>
2023-08-19 12:14:13 +0300

    Fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,7 +13,6 @@ router.register('ingredients', IngredientsViewSet, basename='ingredients')
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
